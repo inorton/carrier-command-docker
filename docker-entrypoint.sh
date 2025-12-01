593580a33653ffd6c69f8047737c54a6cc08333d
Ian Norton <inorton@gmail.com>
2025-12-01 22:39:37 +0000

    Add a logs folder to the logs path

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eux
 
-xvfb-run -a wine "$INSTALL_LOC/dedicated_server.exe" "$@" 2>&1 | tee "$LOG_LOC/console-$(date +%Y-%m-%d-%H:%M:%S).log"
+xvfb-run -a wine "$INSTALL_LOC/dedicated_server.exe" "$@" 2>&1 | tee "$LOG_LOC/logs/console-$(date +%Y-%m-%d-%H:%M:%S).log"
